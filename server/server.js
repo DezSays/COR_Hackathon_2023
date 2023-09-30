a989645f83f2c1212466f3e178986e6e8b9bebfe
johnn4127 <johnn4127@gmail.com>
2023-09-30 16:42:24 -0400

    adds qrcode, request, and intakeform, get routes

--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ require("dotenv").config({ path: "../.env" });
 const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize(process.env.URL);
 const { About_us, Users, Mentors, Mentees } = require("./models");
+const request_tables = require("./models/request_tables");
+const intake_forms = require("./models/intake_forms");
 app.get("/heartbeat", (req, res) => {
   console.log("Heartbeat");
   res.send("heartbeat");
@@ -25,15 +27,35 @@ app.get("/mentors", async (req, res) => {
   const mentorsData = await Mentors.findAll();
   res.send({ mentorsData });
 });
+
 app.get("/mentees", async (req, res) => {
-  try {
     const menteesData = await Mentees.findAll();
     res.send({ menteesData });
-  } catch (err) {
-    console.error(err);
-    res.send(err);
-  }
+  
 });
+
+app.get("/request_form", async (req, res) => {
+    const requestData = await Request_Tables.findAll();
+    res.send({ requestData });
+  
+});
+
+app.get("/qrcode", async (req, res) => {
+    const qrData = await QR_Tables.findAll();
+    res.send({ qrData });
+  
+});
+
+app.get("/intakeform", async (req, res) => {
+    const qrData = await Intake_Forms.findAll();
+    res.send({ qrData });
+  
+});
+
+
+
+
+
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
