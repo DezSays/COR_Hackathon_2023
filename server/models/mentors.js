f51a8aad9ae09db473cdcbe93e5be3ded37061ce
johnn4127 <johnn4127@gmail.com>
2023-09-30 15:54:12 -0400

    adds mentor, mentee, adds aboutus get route

--- server/models/mentors.js
+++ server/models/mentors.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Mentors.belongsTo(models.User, {
+      Mentors.belongsTo(models.Users, {
         foreignKey: 'user_id',
         onDelete: 'CASCADE'
       });
--- server/server.js
+++ server/server.js
@@ -1,12 +1,24 @@
+require('dotenv').config()
 const express = require('express')
 const app = express();
 // const settings = require('./settings')
-require('dotenv').config({ path : '../.env'})
 
+const {Sequelize} = require('sequelize')
+const sequelize = new Sequelize(process.env.URL)
+const {About_us} = require('./models')
 app.get('/heartbeat',(req,res) => {
     console.log('Heartbeat')
     res.send('heartbeat')
 })
+app.get('/aboutus', async (req, res) => {
+    try {
+      const categories = await About_us.findAll();
+      res.send({ categories });
+    } catch (err) {
+      console.error(err);
+      res.send(err);
+    }
+  });
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
