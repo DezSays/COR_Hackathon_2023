9cd7cace2acf3ce34e13bc88212a53e94ab3872c
COR Coding <corcoding@CJCheatamsiPad2.localdomain>
2023-09-30 15:56:55 -0400

    doing mentors endpoint

--- server/server.js
+++ server/server.js
@@ -2,10 +2,15 @@ const express = require('express')
 const app = express();
 // const settings = require('./settings')
 require('dotenv').config({ path : '../.env'})
+const { Mentors, Mentees, Lets_talks} = require('./models')
 
 app.get('/heartbeat',(req,res) => {
     console.log('Heartbeat')
     res.send('heartbeat')
+})
+
+app.get('/mentors', async(req, res) => {
+    const mentors = await Mentors.findAll()
 })
 
 app.listen(3000, () => {
