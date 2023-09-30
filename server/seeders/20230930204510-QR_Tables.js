c4ab2563f2f6325b2bf30da337454fd9991763a6
Kenny Pham <140407813+KennyPham97@users.noreply.github.com>
2023-09-30 16:51:07 -0400

    Dummy data (#93)
    * adding dummy data
    
    * fixed migrations and added dummy data to every table
    
    ---------
    
    Co-authored-by: COR Coding <corcoding@CJCheatamsiPad2.localdomain>

---
+++ server/seeders/20230930204510-QR_Tables.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('QR_Tables', [{
+    content:'QR Table content'
+    }
+  
+  ], {});
+   
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
