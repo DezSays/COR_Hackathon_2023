9de42ff275c7205e6df60626eae204518d006a1a
Kenneth Severe <kennethsevere916@gmail.com>
2023-09-30 13:09:08 -0400

    reset point

--- server/migrations/20230930164038-create-lets-talk.js
+++ server/migrations/20230930164038-create-lets-talk.js
@@ -13,11 +13,11 @@ module.exports = {
         type: Sequelize.STRING
       },
       createdAt: {
-        allowNull: false,
+        allowNull: true,
         type: Sequelize.DATE
       },
       updatedAt: {
-        allowNull: false,
+        allowNull: true,
         type: Sequelize.DATE
       }
     });
