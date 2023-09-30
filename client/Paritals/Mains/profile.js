8e9e5d15ec6cd5f0ca7e883c4fd2d2bda61831a1
Alexander Forde <alexanderforde12@gmail.com>
2023-09-30 15:18:40 -0400

    Profile page being worked on

---
+++ client/Paritals/Mains/profile.js
@@ -0,0 +1,17 @@
+const profileImg=document.getElementById('profileimg');
+const imgInput=document.getElementById('formFile')
+const updateButton=document.getElementById('updatebutton')
+
+updateButton.addEventListener('click',(e)=>{
+    e.preventDefault();
+
+    const selectedImg=imgInput
+    const reader= new FileReader();
+    reader.onload=(e)=>{
+        profileImg.src=event.target.result
+    }
+    
+
+
+
+})
