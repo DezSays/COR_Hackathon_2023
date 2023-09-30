78ccf4436b96a7b20826e409edb4d6fa07b59f0d
Alexander Forde <alexanderforde12@gmail.com>
2023-09-30 11:55:59 -0400

    Edit profile form

---
+++ client/Paritals/Mains/profile.js
@@ -0,0 +1,5 @@
+
+<div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Email address</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+</div>
