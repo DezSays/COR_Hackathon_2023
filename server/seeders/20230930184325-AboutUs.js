a64d0832409de014e2361dc2b6b8d8f305fba0a8
Tony Vu <tony.vuu18@gmail.com>
2023-09-30 15:05:21 -0400

    seeder dummy data

--- server/seeders/20230930184325-AboutUs.js
+++ server/seeders/20230930184325-AboutUs.js
@@ -3,15 +3,10 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    
+     await queryInterface.bulkInsert('About_us', [{
+     content: 'Hello world',
+     }], {});
   },
 
   async down (queryInterface, Sequelize) {
