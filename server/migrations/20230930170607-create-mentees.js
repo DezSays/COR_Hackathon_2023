df3b0b057254da434f466f69a6010ff679ce32a6
COR Coding <corcoding@CJCheatamsiPad2.localdomain>
2023-09-30 13:21:33 -0400

    added Mentees Table

---
+++ server/migrations/20230930170607-create-mentees.js
@@ -0,0 +1,57 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Mentees', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id',
+          as: 'user_id'
+        }
+      },
+      name: {
+        type: Sequelize.TEXT
+      },
+      student_type: {
+        type: Sequelize.STRING
+      },
+      preferred_profession: {
+        type: Sequelize.TEXT
+      },
+      preferred_management: {
+        type: Sequelize.TEXT
+      },
+      preferred_counselor: {
+        type: Sequelize.TEXT
+      },
+      same_gender: {
+        type: Sequelize.BOOLEAN
+      },
+      linkedin_url: {
+        type: Sequelize.TEXT
+      },
+      photo_url: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: true,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: true,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Mentees');
+  }
+};
---
+++ server/models/mentees.js
@@ -0,0 +1,34 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Mentees extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Item.belongsTo(models.User, {
+        foreignKey: 'user_id',
+        onDelete: 'CASCADE'
+      });
+    }
+  }
+  Mentees.init({
+    user_id: DataTypes.INTEGER,
+    name: DataTypes.TEXT,
+    student_type: DataTypes.STRING,
+    preferred_profession: DataTypes.TEXT,
+    preferred_management: DataTypes.TEXT,
+    preferred_counselor: DataTypes.TEXT,
+    same_gender: DataTypes.BOOLEAN,
+    linkedin_url: DataTypes.TEXT,
+    photo_url: DataTypes.TEXT
+  }, {
+    sequelize,
+    modelName: 'Mentees',
+  });
+  return Mentees;
+};
