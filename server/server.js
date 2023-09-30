4a32e9433ea3093482c1e9bf5e49c3715bb9dd16
Santiago Felizzola <santi@LAPTOP-DQG00BQ5.localdomain>
2023-09-30 15:54:49 -0400

    fixed server error & created Users GET route

--- server/server.js
+++ server/server.js
@@ -3,10 +3,18 @@ const app = express();
 // const settings = require('./settings')
 require('dotenv').config({ path : '../.env'})
 
+const { Users } = require('./models')
+
 app.get('/heartbeat',(req,res) => {
     console.log('Heartbeat')
     res.send('heartbeat')
 })
+
+app.get('/users', async(req,res) => {
+    const usersData = await Users.findAll();
+    console.log('Users');
+    res.send(usersData)
+}) 
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
