3fb17a334dc2256fe9b076c5ce5a0630655641d4
Maggie <zach.benton.27@gmail.com>
2023-10-01 23:34:08 -0400

    commit to create branch

---
+++ client/Paritals/Mains/profile.js
@@ -0,0 +1,33 @@
+const express = require ('express');
+const app = express();
+const { user } = require('./models');
+
+app.use(express.json());
+
+//This is the bare bones the fetch function
+app.get ('/api/users', async (req, res) => {
+// const {Firstname, Lastname};
+try {
+
+    const user = await User.findOne({ where: { userid }});
+    res.json(users);
+} catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server error"});
+}
+})
+
+
+// const profileImg=document.getElementById('profileimg');
+
+// const imgInput=document.getElementById('formFile')
+// const profilecard=document.getElementById('profilecard')
+
+// updateButton.addEventListener('click',(e)=>{
+//     e.preventDefault();
+
+//     const selectedImg=imgInput
+//     const reader= new FileReader();
+//     reader.onload=(e)=>{
+//         profileImg.src=e.target.result
+//     };
