9f5155f0068624df17c36cf7f532c98f1a7f3310
Tri Vu <vutriminh@gmail.com>
2023-10-10 14:00:49 -0400

    skeleton of home page

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import React from 'react';
+import { Carousel, Container, Row, Col, Button } from 'react-bootstrap';
 
 const HomePage = () => {
   return (
     <div>
-        HomePage
-    </div>
-  )
-}
 
-export default HomePage
+      {/* Carousel */}
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="image1.jpg"  // Replace with your image source
+            alt="First slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="image2.jpg"  // Replace with your image source
+            alt="Second slide"
+          />
+        </Carousel.Item>
+        {/* Add more Carousel.Items with different images as needed */}
+      </Carousel>
+
+      {/* Introductory Text */}
+      <Container style={{ marginTop: '20px', marginBottom: '20px' }}>
+        <Row>
+          <Col className="text-center">
+            <h2 style={{ color: '#333' }}>Welcome to Our Website!</h2>
+            <p style={{ fontSize: '1.2em', color: '#666' }}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam id dolor id nibh ultricies vehicula ut id elit.
+            </p>
+          </Col>
+        </Row>
+      </Container>
+
+      {/* Call-to-Action Button */}
+      <Container>
+        <Row className="justify-content-center">
+          <Col xs="auto">
+            <Button variant="primary">Get Started</Button>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default HomePage;
