4a24ef32d09e41fc7856d088e389ef724dc38a08
John Nguyen <johnn4127@gmail.com>
2023-10-10 15:04:18 -0400

    Registration (#170)
    * adds registration component
    
    * adds registration component
    
    * adds updates

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Registration from './components/auth/Registration';
 import BaseLayout from './components/layout/BaseLayout'
 // import './App.css'
 import HomePage from './components/HomePage';
 import MenteeRegistration from './components/auth/MenteeRegistration';
+
 
 function App() {
 
@@ -15,10 +17,9 @@ function App() {
             <Route path='/' element={<HomePage /> } />
             <Route path='*' element={<HomePage /> } />
           </Routes>
-           
-          
         </BaseLayout>
       </Router>
+      
     </>
   )
 }
