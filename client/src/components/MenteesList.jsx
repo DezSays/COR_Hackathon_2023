fd7b671d8f74477344b3414ba353056e9eaabdfc
Kenny Pham <140407813+KennyPham97@users.noreply.github.com>
2023-10-10 16:04:17 -0400

    added mentees list (#172)
    Co-authored-by: COR Coding <corcoding@CJCheatamsiPad2.localdomain>

---
+++ client/src/components/MenteesList.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const MenteesList = () => {
+  const [mentees, setMentees] = useState([]);
+
+  const fetchMenteesData = async () => {
+    try {
+      const response = await axios.get('https://cor-hackathon-2023.vercel.app/mentees');
+      setMentees(response.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchMenteesData(); // Call the fetch function
+  }, []);
+
+  return (
+    <div>
+      <h2>Mentees Registered:</h2>
+      <ul>
+        {mentees.map((mentee) => (
+          <li key={mentee.user_id}>
+            <li>Name: {mentee.name}</li>
+            <p>Student Type: {mentee.student_type}</p>
+            <p>Preferred Profession: {mentee.preferred_profession}</p>
+            <p>Preferred Managment: {mentee.preferred_management}</p>
+            <p>Preferred Counselor: {mentee.preferred_counselor}</p>
+            <p>Gender: {mentee.gender}</p>
+            <p>Same Gender?: {mentee.same_gender}</p>
+            <p>LinkedIn URL: {mentee.linkedin_url}</p>
+            <p>Photo: {mentee.photo_url}</p>
+
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default MenteesList;
