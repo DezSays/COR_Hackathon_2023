6f5129f65f5aa8a2128692a90c664061a7f6e38e
John Nguyen <johnn4127@gmail.com>
2023-10-10 13:37:44 -0400

    adds footer (#163)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ function App() {
             <Route path='/' element={<HomePage /> } />
             <Route path='*' element={<HomePage /> } />
           </Routes>
+          
         </BaseLayout>
       </Router>
     </>
