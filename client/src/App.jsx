307a46984a92b5a1278d577deca8fe3527173de4
Tri Vu <vutriminh@gmail.com>
2023-10-10 14:24:54 -0400

    changed import in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import BaseLayout from './components/layout/BaseLayout'
 // import './App.css'
