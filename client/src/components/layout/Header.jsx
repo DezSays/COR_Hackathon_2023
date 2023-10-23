492ab96e2f0c788dc4f2607f422d66508dd485b3
Tri <vutriminh@gmail.com>
2023-10-23 10:32:22 -0400

    basic navbar for now

--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -1,15 +1,39 @@
-import React from 'react'
-import '../../styles/Header.css'
-import CORLOGO from '../../assets/Asset+1.png'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import '../../styles/Header.css';
+import CORLOGO from '../../assets/Asset+1.png';
 
 const Header = () => {
   return (
     <>
       <navbar className="Navbar">
-          <img src={CORLOGO} id="Nav-Brand" alt="" />
+        <Navbar className='nav-bg static-top' expand="lg"> {/* Change fixed-top to static-top */}
+          <Container fluid>
+            <Navbar.Brand style={{ color : 'rgba(0,0,0,1)'}} href="/"><img src={CORLOGO} id="Nav-Brand" alt="" /></Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+              <Nav
+                className="me-auto my-2 my-lg-0"
+                style={{ maxHeight: '100px' }}
+                navbarScroll
+              >
+                <Link className='nav-link-color' to='/'>Home</Link>
+                <Link className='nav-link-color' to="/about-us">About Us</Link>
+                <Link className='nav-link-color' to="/login">Login</Link>
+                <Link className='nav-link-color' to="/mentee-registration">Mentee Registration</Link>
+                <Link className='nav-link-color' to="/mentor-registration">Mentor Registration</Link>
+                <Link className='nav-link-color' to="/registration">Registration</Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
       </navbar>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
