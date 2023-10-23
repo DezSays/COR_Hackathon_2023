620a9e642b7d75d75b1e1a1034ebbced6dfda3b5
John Nguyen <johnn4127@gmail.com>
2023-10-23 11:44:04 -0400

    Mentlist (#179)
    * adds mentlist css
    
    * did mentor regisrer to backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,10 @@ import Registration from './components/auth/Registration';
 import BaseLayout from './components/layout/BaseLayout'
 // import './App.css'
 import HomePage from './components/HomePage';
+import MentorsList from './components/MentorsList';
+import MentorRegistration from './components/auth/MentorRegistration';
+import MenteesList from './components/MenteesList'
+import MenteeRegistration from './components/auth/MenteeRegistration';
 
 
 
@@ -17,6 +21,10 @@ function App() {
           <Routes>
             <Route path='/' element={<HomePage /> } />
             <Route path='*' element={<HomePage /> } />
+            <Route path='/mentors' element ={<MentorsList/>}/>
+            <Route path= '/mentorreg' element ={<MentorRegistration/>}/>
+            <Route path='/mentees' element ={<MenteesList/>}/>
+            <Route path= '/menteereg' element ={<MenteeRegistration/>}/>
           </Routes>
         </BaseLayout>
       </Router>
