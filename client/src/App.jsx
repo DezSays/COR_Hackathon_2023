abd1676a0bfd043cc50dc6fbb79be5d886dc1478
Tri <vutriminh@gmail.com>
2023-10-24 08:00:55 -0400

    changes to HomePage.jsx and App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Registration from './components/auth/Registration';
 import BaseLayout from './components/layout/BaseLayout'
 // import './App.css'
 import HomePage from './components/HomePage';
-
-
-
+import AboutUs from './components/AboutUs';
+import LoginPage from './components/auth/Login';
+import MenteeRegistration from './components/auth/MenteeRegistration';
+import MentorRegistration from './components/auth/MentorRegistration';
+import Registration from './components/auth/Registration';
 
 function App() {
 
@@ -17,6 +18,11 @@ function App() {
           <Routes>
             <Route path='/' element={<HomePage /> } />
             <Route path='*' element={<HomePage /> } />
+            <Route path='about-us' element={<AboutUs /> } />
+            <Route path='login' element={<LoginPage /> } />
+            <Route path='mentee-registration' element={<MenteeRegistration /> } />
+            <Route path='mentor-registration' element={<MentorRegistration /> } />
+            <Route path='registration' element={<Registration /> } />
           </Routes>
         </BaseLayout>
       </Router>
