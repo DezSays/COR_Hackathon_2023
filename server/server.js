96b244ae12111b58e15003a979afe250253fdffa
Dezarea Bryan <dezareabryan@gmail.com>
2023-10-26 16:31:54 -0400

    Swagger test (#188)
    * Implemented Swagger
    
    * Made changed in attempt to fix deployed site bugs.
    
    * Made changed in attempt to fix deployed site bugs.
    
    * Made changed in attempt to fix deployed site bugs.
    
    * Made changed in attempt to fix deployed site bugs.
    
    * Made changed in attempt to fix deployed site bugs.
    
    * resolving merge conflicts
    
    * Made changed in attempt to fix deployed site bugs.
    
    * Made changed in attempt to fix deployed site bugs.
    
    * swagger test

--- server/server.js
+++ server/server.js
@@ -485,6 +485,7 @@ app.use(
     },
   })
   );
+
   app.use(
     cors({
       origin: "*",
@@ -717,6 +718,13 @@ const options = {
   },
   apis: [".//*.js"],
 };
+
+app.use(express.static(__dirname));
+app.use('*.css', (req, res, next) => {
+    res.set('Content-Type', 'text/css');
+    next();
+});
+
 
 const specs = swaggerJsdoc(options);
 app.use(
